4187b17c2bbb7b6a7a74d6c6ec49fd320e50a992
Alison <alisonserafim@gmail.com>
2020-12-28 13:50:52 -0300

    add a command channel

--- src/app.js
+++ src/app.js
@@ -82,7 +82,7 @@ module.exports = class App {
 					message.reply(`PM Command Sended Successfully to: ${ID}`);
 					break;
 				case 'dump':
-					that.dumpChannels();
+					message.reply(`Current Channel ID is: ${message.channel.id}`);
 				break;
 				case 'h':
 					msg = "```Usage: \n !info : Show information about all connected bots. \n !quit <all/username/accountID> : Send Quit command to specific bot, if not defined send to all. \n !relog <all/username/accountID> <time>: Send relog command to specific bot, if not defined send to all. \n !pm <from/all> <to> <message>: Send PM command to bot. \n```";
@@ -193,10 +193,6 @@ module.exports = class App {
 		this.sockets.forEach((client) => {
 			client.write(message);
 		});
-	}
-	
-	dumpChannels() {
-		console.log(this.client.channels.cache); // dump all channels info
 	}
 }
 
